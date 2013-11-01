d295e7589751f40f900a14c7d1010bb19d28aaf1
bgood@velocity.net <bgood@velocity.net@f4836c43-707a-2916-6f80-47259f7129d9>
2013-11-01 01:32:38 +0000

    Finish of native speech on Windows.

--- trunk/Xchecklist/Xchecklist.cpp
+++ trunk/Xchecklist/Xchecklist.cpp
@@ -5,7 +5,7 @@
 //     Michal Navratil
 //     William Good
 //
-//     Ver 0.76 Working to 32/64bit multiplatform
+//     Ver 0.77 Working to 32/64bit multiplatform
 //     X-Plane.org
 //
 //     A plugin to display a clist.txt in widget window
@@ -134,10 +134,10 @@ PLUGIN_API int XPluginStart(
         int		PluginSubMenuItem;
 	int             ChecklistsSubMenuItem;
 
-         xcDebug("Xchecklist: ver 0.76\n");
+         xcDebug("Xchecklist: ver 0.77\n");
 
         /* First set up our plugin info. */
-        strcpy(outName, "Xchecklist ver 0.76");
+        strcpy(outName, "Xchecklist ver 0.77");
         strcpy(outSig, "Michal_Bill.Example.Xchecklist");
         strcpy(outDesc, "A plugin to display checklists in a widget window.");
 
