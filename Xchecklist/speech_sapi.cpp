dc057521dfd63290701e879691ef94e5fdd1ba16
f.josef <f.josef@email.cz>
2014-02-16 16:41:00 +0000

    Added the build4linux script, allowing for linux/windows crosscompilation. Minor changes in respect to crosscompilation wiki page.

--- Xchecklist/speech_sapi.cpp
+++ Xchecklist/speech_sapi.cpp
@@ -4,7 +4,78 @@
 #include <string.h>
 #include <stdio.h>
 #include "utils.h"
+
+
+
+#ifndef __in
+ #define __in
+#endif
+#ifndef __out
+ #define __out
+#endif
+#ifndef __inout
+ #define __inout
+#endif
+#ifndef __in_opt
+ #define __in_opt
+#endif
+#ifndef __out_opt
+ #define __out_opt
+#endif
+#ifndef __inout_opt
+ #define __inout_opt
+#endif
+#ifndef __in_ecount
+ #define __in_ecount(x)
+#endif
+#ifndef __out_ecount
+ #define __out_ecount(x)
+#endif
+#ifndef __inout_ecount
+ #define __inout_ecount(x)
+#endif
+#ifndef __in_bcount
+ #define __in_bcount(x)
+#endif
+#ifndef __out_bcount
+ #define __out_bcount(x)
+#endif
+#ifndef __inout_bcount
+ #define __inout_bcount(x)
+#endif
+#ifndef __out_xcount
+ #define __out_xcount(x)
+#endif
+#ifndef __deref_opt_out
+ #define __deref_opt_out
+#endif
+#ifndef __deref_out
+ #define __deref_out
+#endif
+#ifndef __out_ecount_opt
+ #define __out_ecount_opt(x)
+#endif
+#ifndef __in_bcount_opt
+ #define __in_bcount_opt(x)
+#endif
+#ifndef __out_bcount_opt
+ #define __out_bcount_opt(x)
+#endif
+#ifndef __deref_out_opt
+ #define __deref_out_opt
+#endif
+#ifndef __in_z
+ #define __in_z
+#endif
+#ifndef __out_ecount_part
+ #define __out_ecount_part(x,y)
+#endif
+#ifndef __in_ecount_opt
+ #define __in_ecount_opt(x)
+#endif
+
 #include <sapi.h>
+
 
 static bool active = false;
 
