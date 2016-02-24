c63f00978a8abddf4e1e64ef9d0f893f9045b645
sparker256 <bgood@velocity.net>
2016-02-24 15:16:50 -0500

    Ver 2.16 Added Date & Time to version number for better tracking.

--- src/Xchecklist.cpp
+++ src/Xchecklist.cpp
@@ -13,7 +13,7 @@
 //
 // *********************************************************
 
-#define VERSION_NUMBER "1.16"
+#define VERSION_NUMBER "1.16 build " __DATE__ " " __TIME__
 
 
 #include "XPLMPlugin.h"
