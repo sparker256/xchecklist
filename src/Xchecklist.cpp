30d62dfd8bf44893415803d5ac92adbb0cc43f5d
sparker256 <bgood@velocity.net>
2017-09-15 19:02:28 -0400

    Increased version number to 1.27

--- src/Xchecklist.cpp
+++ src/Xchecklist.cpp
@@ -14,7 +14,7 @@
 //
 // *********************************************************
 
-#define VERSION_NUMBER "1.26 build " __DATE__ " " __TIME__
+#define VERSION_NUMBER "1.27 build " __DATE__ " " __TIME__
 
 
 #include "XPLMPlugin.h"
