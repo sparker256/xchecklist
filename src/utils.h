4291f80b2ecdff42a65f6a64be244a3d2bd9a82d
sparker256 <bgood@velocity.net>
2018-04-24 11:46:47 -0400

    Ver. 1.29 Removed using widget in VR and now using transparent X-Plane 11 new GUI

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,7 @@
 #ifndef XC_UTILS__H
 #define XC_UTILS__H
 
+#include "XPLMDisplay.h"
 #include <stdarg.h>
 
 #ifdef __cplusplus
@@ -18,6 +19,25 @@ void xcSummary(void);
 
 extern int clist_dict_found;
 
+// Variables to be passed to VR window
+extern const char * xcvr_title; // title to be used for the VR window
+extern int xcvr_width; // width of the widget checklist window
+extern int xcvr_height; // height of the widget checklist window
+extern unsigned int xcvr_size; // number of items per checklist page
+extern int xcvr_copilot_controlled[30]; // do we draw the copilot sysbol
+extern int xcvr_item_void[30]; // is the item checkable
+extern int xcvr_left_text_start;
+extern const char * xcvr_text[];
+extern int xcvr_right_text_start;
+extern const char * xcvr_suffix[];
+extern int xcvr_item_checked[];
+
+extern int checkable;
+
+extern int mouse_down_previous;
+extern int mouse_down_check_item;
+extern int mouse_down_next;
+
 #ifdef __cplusplus
 }
 
@@ -27,6 +47,7 @@ void xcLoadDictionaries(std::map<std::string, std::string> &dict);
 
 
 #endif
+
 
 
 
